3895a3e180e0d5d024ec582721b497691c4d858d
chris vaccarello <chris vaccarello>
2016-06-11 02:00:35 -0400

    Basic HighChart ColumnChart example, still need to figure the legend and labeling the "max" value.  It is 2am now, I am going to go to bed and finish this in the morning.

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -10,15 +10,47 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var core_2 = require('@angular/core');
+var angular2_highcharts_1 = require('angular2-highcharts');
 // Turned off encapsulation so that I could style the entire document at the body element
 var AppComponent = (function () {
     function AppComponent() {
         this.client_name = 'Client Name';
         this.customer_name = 'Customer Name';
+        this.options = {
+            colors: ['#5c2946', '#b2ba1c'],
+            chart: {
+                renderTo: 'highchart-columnchart-container',
+                type: 'column'
+            },
+            title: {
+                text: 'Chart Name'
+            },
+            xAxis: {
+                title: {
+                    text: null
+                },
+                categories: ['Nov', 'Dec', 'Jan', 'Fed', 'Mar', 'Apr', 'May', 'Jun']
+            },
+            yAxis: {
+                min: 0,
+                max: 100,
+                title: {
+                    text: null
+                }
+            },
+            series: [{
+                    name: 'year',
+                    data: [42, 40, 37, 40, 45, 50, 60, 77]
+                }, {
+                    name: 'year',
+                    data: [48, 38, 30, 40, 48, 65, 58, 90]
+                }]
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
+            directives: [angular2_highcharts_1.CHART_DIRECTIVES],
             template: require('./app.component.html'),
             styles: [require('./../styles/app.scss')],
             encapsulation: core_2.ViewEncapsulation.None
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
+import { CHART_DIRECTIVES } from 'angular2-highcharts';
 
 // Turned off encapsulation so that I could style the entire document at the body element
 @Component({
 	selector: 'my-app',
+	directives: [CHART_DIRECTIVES],
 	template: require('./app.component.html'),
 	styles: [require('./../styles/app.scss')],
 	encapsulation: ViewEncapsulation.None
@@ -12,4 +14,39 @@ import { ViewEncapsulation } from '@angular/core';
 export class AppComponent {
 	client_name = 'Client Name';
 	customer_name = 'Customer Name';
+
+	constructor() {
+        this.options = {
+			colors: ['#5c2946', '#b2ba1c'],
+			chart: {
+				renderTo: 'highchart-columnchart-container',
+				type: 'column'
+			},
+            title : {
+				text: 'Chart Name'
+			},
+			xAxis: {
+				title: {
+					text: null
+				},
+				categories: ['Nov', 'Dec', 'Jan', 'Fed', 'Mar', 'Apr', 'May', 'Jun']
+	        },
+	        yAxis: {
+				min: 0,
+				max: 100,
+				title: {
+					text: null
+				}
+	        },
+			series: [{
+			    name: 'year',
+			    data: [42, 40, 37, 40, 45, 50, 60, 77]
+			},{
+				name: 'year',
+				data: [48, 38, 30, 40, 48, 65, 58, 90]
+			}]
+        };
+    }
+
+    options: Object;
 }
